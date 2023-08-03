a39d890ef81a041a11734161fec93ce79c9651c7
yuna77 <rbfrhdwn1004@naver.com>
2023-08-03 10:12:36 +0900

    error catch

--- namaranth4/src/test/java/org/namaranth/mapper/UserMapperTests.java
+++ namaranth4/src/test/java/org/namaranth/mapper/UserMapperTests.java
@@ -2,11 +2,9 @@ package org.namaranth.mapper;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.namaranth.domain.UsersVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import lombok.extern.log4j.Log4j;
 
@@ -16,7 +14,7 @@ import lombok.extern.log4j.Log4j;
 public class UserMapperTests {
 	
 	@Autowired
-	private EmailMapper mapper;
+	private UserMapper mapper;
 	
 //	@Test
 //	public void test() {
